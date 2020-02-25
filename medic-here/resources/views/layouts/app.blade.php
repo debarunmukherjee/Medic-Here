dd88fcd35361e0c41c3dfa3e37b785d066809e8c
Debarun Mukherjee <debarun.mukherjee1997@gmail.com>
2020-02-26 01:52:49 +0530

    Header Section added

--- medic-here/resources/views/layouts/app.blade.php
+++ medic-here/resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <link rel="stylesheet" href="{{asset('css/navbar/style.css')}}">
+        <link rel="stylesheet" href="{{asset('css/hero_section/style.css')}}">
 
         <title>{{config('app.name', 'Medic Here')}}</title>
 
--- medic-here/resources/views/pages/index.blade.php
+++ medic-here/resources/views/pages/index.blade.php
@@ -1,5 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hello there!</h1>
+<header>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row hero-bg-col">
+                    <div class="col-md-5">
+                        <div class="container text-center hero-text">
+                            <h1>
+                                <span class="hero-title">We listen</span>
+                            </h1>
+                            <div class="text-left hero-subtitle-container">
+                                <p>to your issues carefully and provide the most accurate treatment within least
+                                    possible time.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="custom-shape text-right">
+                            <img src="{{asset('imgs/doctor-listen.jpg')}}" alt="doctor patient">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="row hero-bg-col">
+                    <div class="col-md-5">
+                        <div class="container text-center hero-text">
+                            <h1>
+                                <span class="hero-title">Trustworthy</span>
+                            </h1>
+                            <div class="text-left hero-subtitle-container">
+                                <p>Our doctors have several years of experience and verified by our board of directors.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="custom-shape text-right">
+                            <img src="{{asset('imgs/Webp.net-resizeimage.jpg')}}" alt="doctor patient">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="row hero-bg-col">
+                    <div class="col-md-5">
+                        <div class="container text-center hero-text">
+                            <h1>
+                                <span class="hero-title">Health Blog</span>
+                            </h1>
+                            <div class="text-left hero-subtitle-container">
+                                <p>Our doctors publish various articles on health and nutrition on a regular basis.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="custom-shape text-right">
+                            <img src="{{asset('imgs/health-blog.jpg')}}" alt="doctor patient">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</header>
+
+<section class="features">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-3"></div>
+            <div class="col-sm-3"></div>
+        </div>
+    </div>
+</section>
 @endsection
